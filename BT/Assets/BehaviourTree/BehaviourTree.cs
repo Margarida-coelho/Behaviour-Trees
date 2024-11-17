d2afe0f15d7d5d3628797699205633cb9388caeb
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-11-17 18:36:48 +0000

    Nodes

---
+++ BT/Assets/BehaviourTree/BehaviourTree.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+using static Unity.VisualScripting.Metadata;
+
+public class BehaviourTree : Node
+{
+    public BehaviourTree()
+    {
+        name = "Tree";
+    }
+
+    public BehaviourTree(string n)
+    {
+        name = n;
+    }
+
+    public void PrintTree()
+    {
+        Debug.Log(this.name);
+
+        for (int i = 0; i < children.Count; ++i)
+        {
+            Debug.Log(children[i].name);
+
+            for (int y = 0; y < children[i].children.Count; ++y)
+            {
+                Debug.Log(children[i].children[y].name);
+            }
+        }
+    }
+}
